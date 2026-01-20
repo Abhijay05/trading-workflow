a202224c1a45a027bba72aa5d4234b432329d960
Abhijay05 <abhijay_b@mt.iitr.ac.in>
2026-01-20 19:26:23 +0530

    createWorkflowSchema

--- trading-turborepo/packages/common/types/index.ts
+++ trading-turborepo/packages/common/types/index.ts
@@ -7,3 +7,25 @@ export const SigninSchema = z.object({
   username: z.string().min(3).max(100),
   password: z.string(),
 });
+export const CreateWorkflowSchema = z.object({
+  nodes: z.array(
+    z.object({
+      type: z.string(),
+      id: z.string(),
+      data: z.object({
+        kind: z.enum(["action", "trigger"]),
+        metadata: z.any(),
+        label: z.string(),
+      }),
+      position: z.object({
+        x: z.number(),
+        y: z.number(),
+      }),
+    }),
+  ),
+  edges: z.object({
+    id: z.string(),
+    source: z.string(),
+    target: z.string(),
+  }),
+});
